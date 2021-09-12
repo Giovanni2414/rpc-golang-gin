e8e1e6c457196b93003c4a8f65406a91f9042d10
Giovanni Mosquera <Giovanni2414g@gmail.com>
2021-09-12 18:19:51 -0500

    Login repared

--- main.go
+++ main.go
@@ -3,29 +3,30 @@ package main
 // Dependencies
 import (
 	//"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-    "net/http"
-    //"os"
+	//"os"
 )
 
 // User struct to represent data about users
 type User struct {
-    Username			string  `json:"username"`
-	Password			string  `json:"password"`
-	ConfirmPassword		string  `json:"confirmpassword"`
-	Firstname			string  `json:"firstname"`
-	Lastname			string  `json:"lastname"`
-	Day					int		`json:"day"`
-	Month				int		`json:"month"`
-	Year				int		`json:"year"`
+	Username        string `json:"username"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirmpassword"`
+	Firstname       string `json:"firstname"`
+	Lastname        string `json:"lastname"`
+	Day             int    `json:"day"`
+	Month           int    `json:"month"`
+	Year            int    `json:"year"`
 }
 
-var userLogged []User = []User{}
+var userLogged = []User{}
 
 // Users slice to seed record users data.
 var users = []User{
-    {Username: "xGiovanni", Password: "nike4545", ConfirmPassword: "nike4545", Firstname: "Giovanni", Lastname: "Mosquera", Day: 27, Month: 07, Year: 2001},
-    {Username: "Juseros9", Password: "contraseña", ConfirmPassword: "contraseña", Firstname: "Sebastián", Lastname: "Rodriguez", Day: 16, Month: 11, Year: 2002},
+	{Username: "xGiovanni", Password: "nike4545", ConfirmPassword: "nike4545", Firstname: "Giovanni", Lastname: "Mosquera", Day: 27, Month: 07, Year: 2001},
+	{Username: "Juseros9", Password: "contraseña", ConfirmPassword: "contraseña", Firstname: "Sebastián", Lastname: "Rodriguez", Day: 16, Month: 11, Year: 2002},
 }
 
 // Creating the handler functions
@@ -33,7 +34,7 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLFiles("login.html", "register.html", "view.html")
 	router.GET("/", defaultRedirect)
-    router.GET("/users", loadViewLogin)
+	router.GET("/users", loadViewLogin)
 	router.POST("/users", loginUser)
 	router.GET("/users/register", loadViewRegister)
 	router.POST("/users/register", userRegister)
@@ -47,45 +48,45 @@ func defaultRedirect(c *gin.Context) {
 }
 
 func loadViewLogin(c *gin.Context) {
-	if(userLogged != nil) {
-		c.HTML(http.StatusOK, "view.html", gin.H {
-			"user": userLogged,
+	if len(userLogged) != 0 {
+		c.HTML(http.StatusOK, "view.html", gin.H{
+			"user":  userLogged,
 			"users": users,
 		})
 		return
 	} else {
-		c.HTML(http.StatusOK, "login.html", gin.H {
+		c.HTML(http.StatusOK, "login.html", gin.H{
 			"message": " ",
 		})
 	}
 }
 
 func loginUser(c *gin.Context) {
-	username := c.PostForm("Username");
-	password := c.PostForm("Password");
+	username := c.PostForm("Username")
+	password := c.PostForm("Password")
 
-	if(len(username) > 0 && len(password) > 0) {
+	if len(username) > 0 && len(password) > 0 {
 		for _, a := range users {
 			if a.Username == username {
 				if a.Password == password {
 					userLogged := a
-					c.HTML(http.StatusOK, "view.html", gin.H {
+					c.HTML(http.StatusOK, "view.html", gin.H{
 						"username": userLogged.Username,
-						"users": users,
+						"users":    users,
 					})
 					return
 				}
-				c.HTML(http.StatusOK, "login.html", gin.H {
+				c.HTML(http.StatusOK, "login.html", gin.H{
 					"message": "Incorrect Password",
 				})
 				return
 			}
 		}
-		c.HTML(http.StatusOK, "login.html", gin.H {
+		c.HTML(http.StatusOK, "login.html", gin.H{
 			"message": "This user doesn't exist",
 		})
 	} else {
-		c.HTML(http.StatusOK, "login.html", gin.H {
+		c.HTML(http.StatusOK, "login.html", gin.H{
 			"message": "Fill the camps for access",
 		})
 	}
@@ -100,6 +101,6 @@ func userRegister(c *gin.Context) {
 }
 
 func logoutUser(c *gin.Context) {
-	userLogged = []User{};
+	userLogged = []User{}
 	c.Redirect(http.StatusMovedPermanently, "/users")
 }
